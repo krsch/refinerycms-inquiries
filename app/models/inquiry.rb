9eb63971bb064acaf4d50c3499e7deec40489d73
Alexey Kreshchuk <akrsch@gmail.com>
2011-12-01 00:38:40 +0400

    added categories to user

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -8,7 +8,7 @@ class Inquiry < ActiveRecord::Base
 
   default_scope :order => 'created_at DESC' # previously scope :newest
 
-  attr_accessible :name, :message, :email, :isprivate
+  attr_accessible :name, :message, :email, :isprivate, :inquiry_category_id
   attr_accessible :status, :answer, :recipient, :as => :admin
   
   belongs_to :inquiry_category
